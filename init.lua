a9f2483ea67e6fcebe7b471d919f65c829ee42d4
cod1r <jasonho353@gmail.com>
2024-06-10 14:53:31 -0500

    clangd is buttcheeks

--- init.lua
+++ init.lua
@@ -183,18 +183,7 @@ require'lspconfig'.rust_analyzer.setup{
 require'lspconfig'.tsserver.setup{
 	root_pattern("tsconfig.json", "package.json", "jsconfig.json", ".git")
 }
-require'lspconfig'.clangd.setup{
-	single_file_support = false,
-  root_dir = root_pattern(
-		'.clangd',
-		'.clang-tidy',
-		'.clang-format',
-		'compile_commands.json',
-		'compile_flags.txt',
-		'configure.ac',
-		'.git'
-	)
-}
+require'lspconfig'.ccls.setup{}
 require'lspconfig'.ocamllsp.setup{}
 -- vim.cmd [[autocmd! CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]]
 vim.cmd [[autocmd BufEnter *.tsx set filetype=typescriptreact]]
